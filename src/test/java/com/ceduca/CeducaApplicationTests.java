b2816ca86a6c56aab5e0917ed0317d8b14f4b6a7
murillomsouza <murillosouza997@gmail.com>
2026-05-22 10:27:24 -0300

    test: fix package structure for Spring Boot test

---
+++ src/test/java/com/ceduca/CeducaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceduca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeducaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
