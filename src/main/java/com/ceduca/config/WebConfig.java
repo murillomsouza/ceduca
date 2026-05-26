08e6656d9d2b99e02847a080194bd0ddea68d4cf
murillomsouza <murillosouza997@gmail.com>
2026-05-26 10:03:32 -0300

    config: add CORS configuration for frontend integration

---
+++ src/main/java/com/ceduca/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.ceduca.config;
+
+import org.jspecify.annotations.NonNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
